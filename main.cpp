fb9552dfaf9fba96fd1d9d4f31025513101565ee
radwano <mateuszradwan@protonmail.com>
2025-08-03 08:34:08 +0200

    Properly added Complex.h/.cpp to project, added short demo into main()

--- main.cpp
+++ main.cpp
@@ -8,7 +8,17 @@ using namespace std;
 
 int main()
 {
-	Complex z1(2,3);
-	z1 += Complex(2, 0);
-	cout << z1.mod();
+			//Short demo
+
+	Matrix A(2, 2);
+	A(1, 1) = -1;	A(1, 2) = 3;	// [-1 3 ]
+	A(2, 1) = 3;	A(2, 2) = 4;	// [ 3 4 ]
+
+	Complex z1(0, sqrt(3)/2);
+	z1 += Complex(0.5, 0);	//	z1 = 1/2 + (sqrt3)/2i
+
+	A(1, 1) += z1.mod();	//	|z1| = 1
+
+	cout << A.det();	//	-9
+	return 0;
 }
